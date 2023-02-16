db75e28578a6495a1baa68046fc0d659d445ae99
onlineservicemarketing <93332126+utaramas@users.noreply.github.com>
2023-02-16 23:46:01 +0700

    update code

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+//Pass through files
+eleventyConfig.addPassthroughCopy('./src/style.css');
+eleventyConfig.addPassthroughCopy('./src/assets');
+
+//Date Clean up
+eleventyConfig.addFilter("postDate", (dateObj) => {
+return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+});
+
+return {
+	dir: {
+		input: "src"
+	}
+}
+
+};
