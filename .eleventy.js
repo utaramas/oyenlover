b1b565cea6ed8fc1df044a049ce5eeab156bbe78
onlineservicemarketing <93332126+utaramas@users.noreply.github.com>
2023-02-18 11:06:05 +0700

    update codes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,14 @@
+const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
+
+module.exports = function(eleventyConfig) {
+  // If you have other `addPlugin` calls, it’s important that UpgradeHelper is added last.
+  eleventyConfig.addPlugin(UpgradeHelper);
+};
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy
   ("./src/style.css");
-  
+
   return {
     dir: {
       input: "src",
